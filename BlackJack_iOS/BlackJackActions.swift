3a593ce1f040a8dbbe88a40ad9ae4d7a56fe9c56
GeoSau <saucedogeorge9@gmail.com>
2019-12-02 11:23:42 -0600

    Logic
    Logic

--- BlackJack_iOS/BlackJackActions.swift
+++ BlackJack_iOS/BlackJackActions.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class BlackJackActions
 {
+    var deck: Deck!
+    var player: Player!
+    var dealer: Player!
+    var blackJackValue: Int
+    
+    init(deck: Deck, player: Player, dealer: Player){
+        self.deck = deck
+        self.player = player
+        self.dealer = dealer
+        blackJackValue = 21
+    }
+    
     func Deal(player: Player, dealer: Player)
     {
         player.drawCards(numCards: 2)
@@ -30,5 +42,28 @@ class BlackJackActions
     {
         //betAmount *= 2
         return betAmount
+    }
+    
+    func gameConditions(amountBet: Int, player: Player, dealer: Player)-> Array<Any>{
+        var playerHandValue: Int = player.calculateBlackjackHandValue()
+        var dealerHandValue: Int = dealer.calculateBlackjackHandValue()
+        var array = [String]()
+        
+        if player.calculateBlackjackHandValue() > blackJackValue{
+            array[0] = "You have busted!"
+        }
+        else if playerHandValue < blackJackValue && dealerHandValue > blackJackValue{
+            array[0] = "Dealer has busted, You Win!"
+        }
+        else if playerHandValue < blackJackValue && playerHandValue == dealerHandValue{
+            array[0] = "It is a push. You get your money back."
+        }
+        else if playerHandValue < blackJackValue && dealerHandValue > playerHandValue{
+            array[0] = "Dealer Wins"
+        }
+        else{
+            array[0] = "You Win"
+        }
+        return array
     }
 }
